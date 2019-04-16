5c9ab9f62fb02381c21f322d68581b4845f9924f
HectorBritoDev <hectorbritodev@gmail.com>
2019-04-16 08:48:49 -0400

    Review Model

--- app/Review.php
+++ app/Review.php
@@ -28,8 +28,14 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Review extends Model
 {
+    protected $fillable = ['course_id', 'user_id', 'rating', 'comment'];
     public function course()
     {
         return $this->belongsTo('App\Course');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
     }
 }
