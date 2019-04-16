c794e6a69b8e79bccd21fab5d6dc08717fa004b5
HectorBritoDev <hectorbritodev@gmail.com>
2019-04-16 08:52:28 -0400

    web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,22 @@
 Route::get('login/{id}', 'Auth\LoginController@redirectToProvider')->name('social_auth');
 Route::get('login/{driver}/callback', 'Auth\LoginController@handleProviderCallback');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/set_languate/{lang}', 'Controller@setLanguage')->name('set_language');
+
+Route::get('/images/{path}/{attachment}', function ($path, $attachment) {
+    $file = sprintf('storage/%s/%s', $path, $attachment);
+
+    if (File::exists($file)) {
+        return Image::make($file)->response();
+    }
+
+    //MOSCA CON EL STORAGE LINK
+});
+
+Route::group(['prefix' => 'courses'], function () {
+    Route::get('/{course}', 'CourseController@show')->name('courses.detail');
+});
