69e55e5b7d1b955a9d7090bf40abeb6fde10eb64
HectorBritoDev <hectorbritodev@gmail.com>
2019-04-16 08:50:05 -0400

    Student Model

--- app/Student.php
+++ app/Student.php
@@ -28,6 +28,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Student extends Model
 {
+    protected $fillable = ['user_id'];
+
     public function courses()
     {
         return $this->belongsToMany('App\Course');
