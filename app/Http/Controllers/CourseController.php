77608d7054363fb9ed8975e68ad7742478ea4304
HectorBritoDev <hectorbritodev@gmail.com>
2019-04-27 21:53:52 -0400

    Course Controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Course;
+use App\Review;
+use App\Mail\NewStudentInCourse;
+use Illuminate\Support\Facades\Mail;
 
 class CourseController extends Controller
 {
@@ -40,5 +43,39 @@ class CourseController extends Controller
         $related = $course->relatedCourses();
 
         return view('courses.detail', compact('course', 'related'));
+    }
+
+    public function inscribe(Course $course)
+    {
+        // return new NewStudentInCourse($course, 'admin');
+        $course->students()->attach(auth()->user()->student->id);
+
+        Mail::to($course->teacher->user)->send(new NewStudentInCourse($course, auth()->user()->name));
+
+        return back()->with('message', ['success', __('Inscrito correctamente al curso')]);
+    }
+
+    public function subscribed()
+    {
+        //TAMBIEN SIRVE auth()->user()->student->courses
+        //PERO ESTA FORMA ES NUEVA Y POR ESO SE USA.
+        $courses = Course::whereHas('students', function ($query) {
+            $query->where('user_id', auth()->user()->id);
+        })->get();
+
+        return view('courses.subscribed', compact('courses'));
+    }
+
+    public function addReview()
+    {
+
+        Review::create([
+            'user_id'=> auth()->id(),
+            'course_id'=> request('course_id'),
+            'rating' => (int)request('rating_input'),
+            'comment'=> request('message'),
+        ]);
+
+        return back()->with('message',['success',__('Muchas gracias por valorar el curso')]);
     }
 }
