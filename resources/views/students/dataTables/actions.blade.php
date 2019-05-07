a3ded37d5381ea467104394ff8c8014c04b8c77f
HectorBritoDev <hectorbritodev@gmail.com>
2019-05-07 18:53:27 -0400

    Send message action button

---
+++ resources/views/students/dataTables/actions.blade.php
@@ -0,0 +1,5 @@
+<a href="#" data-target="#appModal" title="{{ __('Enviar Mensaje') }}" data-id="{{ $user['id'] }}"
+    class="btn btn-primary btnEmail">
+    <i class="fa fa-envelope-square"></i>
+
+</a>
